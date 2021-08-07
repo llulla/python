79bb3190981f49f7594110aa7ecf17e95834455b
호에에에에에\roqja <roqja3319@gmail.com>
2021-08-07 21:27:58 +0900

    study_210807

--- quiz.py
+++ quiz.py
@@ -22,13 +22,101 @@
 
 예) 생성된 비밀번호 nav51!
 """
-add = "http://naver.com"
-add1 = add[7:]
-print(add1)
+# 내가 한거
+# add = "http://naver.com"
+# add1 = add[7:]
+# print(add1)
 
-print(add1.find("."))
-add2 = add1[:5]
-print(add2)
+# print(add1.find("."))
+# add2 = add1[:5]
+# print(add2)
 
-add3 = add2[:3] + str(len(add2)) + str(add2.count("e"))+"!"
-print(add3)
+# add3 = add2[:3] + str(len(add2)) + str(add2.count("e"))+"!"
+# print(add3)
+
+# #풀이
+
+# url = "http://naver.com"
+# my_str = url.replace("http://","") #규칙 1
+# print(my_str)
+# my_str = my_str[:my_str.index(".")] #규칙 2
+# print(my_str)
+# password = my_str[:3] + str(len(my_str))+str(my_str.count("e"))+"!"
+# print("{0}의 비밀번호는 {1}입니다.".format(url, password))
+
+
+
+"""quiz) 당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
+참석률을 높이기 위해 댓글 이벤트를 진행하기로 하였습니다.
+댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
+추첨 프로그램을 작성하세요
+
+조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20이라고 가정
+조건2 : 댓글 내용과 상관없이 무작위로 추첨하되 중복 불가
+조건3 : random 모듈의 shuffle과 sample을 활용
+
+(출력 예제)
+--당첨자 발표--
+치킨 당첨자 : 1
+커피 당첨자 : [2,3,4]
+--축하합니다.--
+
+활용 예제
+from random import *
+lst = [1,2,3,4,5]
+print(lst)
+shuffle(lst)
+print(lst)
+print(sample(lst,1) #
+"""
+
+# from random import *
+# users = range(1,21)
+# users = list(users)
+# print(users)
+
+# shuffle(users)
+# winner = sample(users,4)
+
+# print(winner, type(winner))
+
+# print("--당첨자 발표--\n치킨 당첨자 : {}\n커피 당첨자 : {}\n--축하합니다.--".format(winner[0], winner[1:]))
+
+"""quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사입니다.
+50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오
+
+조건1 : 승객별 운행 소요 시간은 5~50분 사이의 난수로 정해집니다.
+조건2 : 당신은 소요시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
+
+(출력문 예제)
+[0] 1번째 손님 (소요시간 : 15분)
+[ ] 2번째 손님 (소요시간 : 50분)
+[0] 3번째 손님 (소요시간 : 5분)
+...
+[ ] 50번째 손님 (소요시간 : 16분)
+
+총 탑승 승객 : 2분 """
+
+# from random import *
+# count=0
+
+# for client_no in range(1,51):
+#     times=randint(5,50)
+#     if times <=15:
+#         print("[o] {}번째 손님 (소요시간 : {}분)".format(client_no, times))
+#         count+=1
+#     else:
+#         print("[ ] {}번째 손님 (소요시간 : {}분)".format(client_no, times))
+# print("총 탑승 승개 : {}분".format(count))
+
+#풀이
+from random import *
+cnt=0 # 총 탑승 승객 수
+for i in range(1,51): #1~50 이라는 수(승객)
+    time=randrange(5,51) # 5분~50분 소요시간
+    if 5<= time <=15: #5분 ~15분 이내의 손님, 탑승 승객 수 증가 처리
+        print("[o] {0}번째 손님 (소요시간 : {1}분".format(i,time))
+        cnt+=1
+    else :
+        print("[ ] {0}번째 손님 (소요시간 : {1}분".format(i,time))
+print("총 탑승 승객 : {0}분".format(cnt))
--- study.py
+++ study.py
@@ -97,3 +97,222 @@
 # print("Redd\bApple")
 # print("Red\tApple")
 
+# 리스트 []
+
+# # 지하철 칸별로 10명, 20명, 30명
+# subway1 =10
+# subway2 =20
+# subway3 =30
+
+# subway =[10,20,30]
+# print(subway)
+
+# subway=["유재석", "조세호", "박명수"]
+# print(subway.index("유재석"))
+
+# subway.append("하하") #리스트 맨 뒤에 추가
+
+# subway.insert(1,"정형돈") #인덱스 번호로 지정해서 추가
+# print(subway)
+
+# print(subway.pop()) #리스트 맨뒤에서 하나 삭제
+
+# # 같은 이름의 사람이 몇명있는지
+# subway.append("유재석")
+# print(subway.count("유재석"))
+
+# #정렬
+# num_list = [5,3,2,6,4,1]
+# num_list.sort()
+# print(num_list)
+
+# num_list.reverse() #순서 뒤집기 가능
+# print(num_list)
+
+# num_list.clear() # 전체 삭제
+# print(num_list)
+
+# mix_list = ["조세호", 28, True]
+
+# #리스트 확장
+# num_list.extend(mix_list)
+# print(num_list)
+
+#사전
+# cabinet = {3:"유재석", 100:"김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(3))
+# #print(cabinet[5]) #값이 없을경우 바로 에러종료
+# print(cabinet.get(5))
+# print(3 in cabinet) # tree
+# print(5 in cabinet) = 
+
+# cabinet = {"A-3":"유재석", 100:"김태호"}
+
+# print(cabinet)
+# cabinet["A-3"] = "김종국"
+# cabinet["C-20"]="조세호"
+# print(cabinet)
+
+# #간 손님
+# del cabinet["A-3"]
+# print(cabinet)
+
+# #key들만 출력
+# print(cabinet.keys())
+
+# #value들만 출력
+# print(cabinet.values())
+
+# #key, value 쌍으로 출력
+# print(cabinet.items())
+
+# #싹 지우기
+# cabinet.clear()
+# print(cabinet)
+
+
+# #튜플 
+# #   추가 제거 불가능, 고정된 값에 대해서만 사용가능
+
+# menu = ("돈까스", "치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+# (name, age, hobby) = ("김종국", 20,"코딩")
+# #이렇게 구조체 개념으로 사용 가능
+# print(name, age, hobby)
+
+
+# #집합 (set)
+# #   중복 안됨, 순서 없음
+
+# my_set= {1,2,3,4,5,6}
+# print(my_set)
+# java = {"유재석", "김태호", "양세형"}
+# python = set(["유재석", "박명수"])
+
+# #교집합
+# print(java & python)
+# print(java.intersection(python))
+
+# #합집합
+# print(java | python)
+# print(java.union(python))
+
+# #차집합 (java는 가능 python 불가)
+# print(java-python)
+# print(java.difference(python))
+
+# #python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# #java를 까먹음
+# python.remove("김태호")
+# print(java)
+
+
+# # 자료구조의 변경
+# #커피숍
+# menu = {"커피", "우유", "주스"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu = tuple(menu)
+# print(menu, type(menu))
+
+# menu = set(menu)
+# print(menu, type(menu))
+
+
+"""제어문"""
+
+
+# #if
+
+# weather = input("오늘 날씨는 어때요?")
+# if weather =="비" or weather=="눈":
+#     print("우산을 챙기세요")
+# elif weather =="미세먼지":
+#     print("마스크를 챙기세요")
+# else :
+#     print("준비물 필요 없어요")
+
+# temp = int(input("기온은 어때요? "))
+# if 30<=temp:
+#     print("더워요")
+# elif 10<= temp and temp <30:
+#     print("괜찮네요")
+# elif 0<= temp <10:
+#     print("외투를 챙기세요")
+# else:
+#     print("너무 추워요.3")
+
+
+# #for
+# for waiting_no in range(1,16): # randrange()
+#     print("대기번호 : {0}".format(waiting_no))
+
+# starbucks = ["아이언맨", "토르","그루트"]
+# for customer in starbucks:
+#     print("{}님, 주문하신 음료 나왔습니다.".format(customer))
+
+# #while
+
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비되었습니다. {1}번 남았어요".format(customer,index))
+#     index -=1
+#     if index ==0:
+#         print("커피 폐기처분")
+
+# #무한 루프
+# customer ="ironman"
+# index =1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. \t 호출 {1}회".format(customer,index))
+# index += 1
+
+# customer = "thor"
+# person ="Unknown"
+
+# while person != customer:
+#     print("{0}, 커피가 준비 되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요? ")
+
+# # continue, break
+
+# absend = [2,5] # 결석
+# no_book =[7]
+# for student in range (1,11):
+#     if student in absend:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 끝, {0}은 교무실로 오세요".format(student))
+#         break
+#     print("{0}, 책을 일어봐". format(student))
+    
+
+
+# #출석번호가 1,2,3,4 dkvdp 100을 붙이기로함.
+
+# student = [1,2,3,4,5]
+# print(student)
+# student=[i+100 for i in student]
+# print(student)
+
+# #학생이름을 길이로 변환
+# students =["iron man", "thor", "groot"]
+# students =[len(i) for i in students]
+# print(students)
+
+# #학생이름을 대문자로 변환
+# students =["iron man", "thor", "groot"]
+# students =[i.upper() for i in students]
+# print(students)
