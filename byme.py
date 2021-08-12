328c2fc5dd7a1d542f37438a389531746b1192b6
호에에에에에\roqja <roqja3319@gmail.com>
2021-08-12 17:52:33 +0900

    end_python_2_210812

---
+++ byme.py
@@ -0,0 +1,3 @@
+def sign():
+    print("이 프로그램은 llulla에 의해 만들어졌습니다.\n\
+github : https://github.com/llulla")
--- quiz.py
+++ quiz.py
@@ -268,33 +268,48 @@ while(True):
         chicken -=order"""
 
 
-chicken = 10
-waiting = 1
+# chicken = 10
+# waiting = 1
 
-class soldout(Exception):
-    def __init__(self, msg):
-        self.msg =msg
-    def __str__(self):
-        return self.msg
+# class soldout(Exception):
+#     def __init__(self, msg):
+#         self.msg =msg
+#     def __str__(self):
+#         return self.msg
 
-while(True):
-    try:
-        print("남은 치킨 : {0}]".format(chicken))
-        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
-        if order > chicken: 
-           print("재료가 부족합니다.")
-        elif order <= 0:
-           raise ValueError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
-        else:
-            print("[대기번호 {0}] {1}마리 주문이 완료되었습니다."\
-                .format(waiting, order))
-            waiting +=1
-            chicken -= order
-            if chicken == 0:
-                raise soldout("재고가 소진되어 더 이상 주문을 받지 않습니다.")
-    except ValueError:
-        print("잘못된 값을 입력하였습니다.")
-    except soldout:
-        break
+# while(True):
+#     try:
+#         print("남은 치킨 : {0}]".format(chicken))
+#         order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+#         if order > chicken: 
+#            print("재료가 부족합니다.")
+#         elif order <= 0:
+#            raise ValueError("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         else:
+#             print("[대기번호 {0}] {1}마리 주문이 완료되었습니다."\
+#                 .format(waiting, order))
+#             waiting +=1
+#             chicken -= order
+#             if chicken == 0:
+#                 raise soldout("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except soldout:
+#         break
     
-    
+"""quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+조건 : 모듈 파일명은 byme.py로 작성
+
+(모듈 사용 예제)
+import byme
+byme.sign()
+
+(출력예제)
+이 프로그램은 llulla에 의해 만들어졌습니다.
+github : https://github.com/llulla
+
+"""
+import byme
+
+byme.sign()
